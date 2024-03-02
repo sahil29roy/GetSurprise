8a97f12c57edb57b33a1bd6c7a37973268e52fae
Sahil <160802995+sahil29roy@users.noreply.github.com>
2024-03-02 16:14:13 +0530

    Update index.js
    changing port

--- index.js
+++ index.js
@@ -2,18 +2,20 @@ import express from "express";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import bodyParser from "body-parser";
-const port = 3000;
-const __dirname = dirname(fileURLToPath(import.meta.url));
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }))  
+app.use(bodyParser.urlencoded({ extended: true }));
 
 var userIsAuthorised = false;
+
 function check(req, res, next) {
     const username = req.body["username"];
     const password = req.body["password"];
-    if (username === "sahil08roy" && password === "sahil123") { userIsAuthorised = true; }
+    if (username === "sahil08roy" && password === "sahil123") {
+        userIsAuthorised = true;
+    }
     next();
 }
 
@@ -23,16 +25,17 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
 
-app.get("/submitform", (req, res) => {   // post
+app.post("/submitform", (req, res) => {
     if (userIsAuthorised) {
         // console.log(userIsAuthorised);
-        res.render(__dirname + "/welcome.ejs", { name: req.body["username"] });  //__dirname + 
+        res.render(__dirname + "/welcome.ejs", { name: req.body["username"] }); //__dirname +
     } else {
         res.redirect('/');
     }
 });
 
-
+const port = process.env.PORT || 3000;
+//port
 app.listen(port, () => {
-    console.log(`Server is running at port : ${port}`)
-})
+    console.log(`Server is running on port: ${port}`);
+});
